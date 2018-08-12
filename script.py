a20fa06bfae87193dafa9ccebb0166e63597e455
William Song <30965609+Freakwill@users.noreply.github.com>
2018-08-12 12:50:09 +0800

    Add files via upload
    add subparsers

--- script.py
+++ script.py
@@ -4,12 +4,26 @@
 import argparse
 
 import load
+import search
 
-parser = argparse.ArgumentParser(description='Load av')
-parser.add_argument('-s', dest='style', action='store', metavar='STLYE', default=None, help='the style of the movies')
-parser.add_argument('-i', dest='index', action='store', metavar='INDEX', nargs='+', type=int, help='the indexes')
-parser.add_argument('-f', dest='folder', action='store', metavar='FOLDER', default=None, help='the folder where the movies are saved')
+def ld(args):
+    load.load(args.index, args.folder)
+
+def srch(args):
+    search.search(args.keyword, args.style)
+
+parser = argparse.ArgumentParser(description='Load avs')
+subparsers = parser.add_subparsers(title='loading/searching command')
+parser_l = subparsers.add_parser('load', help='Load avs with indexes')
+parser_l.add_argument('-i', dest='index', action='store', metavar='INDEX', nargs='+', type=int, help='the indexes')
+parser_l.add_argument('-f', dest='folder', action='store', metavar='FOLDER', default=None, help='the folder where the movies are saved')
+parser_l.add_argument('-o', dest='operator', action='store', metavar='OPERATOR', default=ld)
+
+parser_s = subparsers.add_parser('search', help='Search avs with keywords')
+parser_s.add_argument('-k', dest='keyword', action='store', metavar='KEYWORD', default=None, help='any keyword')
+parser_s.add_argument('-s', dest='style', action='store', metavar='STLYE', default=None, help='the style of the movies')
+parser_s.add_argument('-o', dest='operator', action='store', metavar='OPERATOR', default=srch)
 
 args = parser.parse_args()
 
-load.load(args.index, args.folder)
+args.operator(args)
